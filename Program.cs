e9147568c64d70ea15838cdaf685780a63648097
DBJ <dbjdbj@gmail.com>
2010-06-16 13:02:28 +0100

    Some additions while working on 'branch_2'

--- Program.cs
+++ Program.cs
@@ -23,6 +23,12 @@ namespace GITEST1
                 "My 'DomainManager' says the EntryAssembly full name is: {0}",
                 AppDomain.CurrentDomain.DomainManager.EntryAssembly.FullName
             ));
+
+            /* now I am in branch_2 */
+            Console.WriteLine(string.Format(
+                "I think this is enough info for you: {0}",
+                System.Security.Principal.WindowsIdentity.GetCurrent().Name
+                ));
         }
     }
 }
