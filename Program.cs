a9e990493fdf3c946e15e2d3a23c6421eca113f9
DBJ <dbjdbj@gmail.com>
2010-06-16 13:58:11 +0100

    source streamlined

--- Program.cs
+++ Program.cs
@@ -9,34 +9,14 @@ namespace GITEST1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( string.Format(
-                "Hi, my friendly name is: {0}",
-                AppDomain.CurrentDomain.FriendlyName
-            ));
-            /*
-             * Here I know I am "back" to branch "master"
-             */
-            Console.WriteLine(string.Format(
-            /* I am now in branch_4 
-             * In git please observe the POINT at which I have decided to create this branch
-             * which is 'way back' in time
-             */
-                "My 'DomainManager' says the EntryAssembly full name is: {0}",
-                AppDomain.CurrentDomain.DomainManager.EntryAssembly.FullName
-            ));
-            /*
-             * Here I started additions while in 'branch_1'
-             */
-            Console.WriteLine(string.Format(
-                "And, my id is: {0}",
-                AppDomain.CurrentDomain.Id
-
-            /* now I am in branch_2 */
-            Console.WriteLine(string.Format(
-                "I think this is enough info for you: {0}",
-                System.Security.Principal.WindowsIdentity.GetCurrent().Name
-                ));
-            ));
+            Console.WriteLine( string.Format("Hi, my friendly name is: {0}",
+                AppDomain.CurrentDomain.FriendlyName));
+            Console.WriteLine(string.Format("My 'DomainManager' says the EntryAssembly full name is: {0}",
+                AppDomain.CurrentDomain.DomainManager.EntryAssembly.FullName));
+            Console.WriteLine(string.Format("And, my id is: {0}",
+                AppDomain.CurrentDomain.Id));
+            Console.WriteLine(string.Format("I think this is enough info for you: {0}",
+                System.Security.Principal.WindowsIdentity.GetCurrent().Name));
         }
     }
 }
