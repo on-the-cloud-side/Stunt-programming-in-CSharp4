d8b50dac17ed79d96b059b97f511262a20f21408
DBJ <dbjdbj@gmail.com>
2010-06-16 12:47:21 +0100

    Additions while inside 'master' branch

--- Program.cs
+++ Program.cs
@@ -16,6 +16,13 @@ namespace GITEST1
                 "Hi, my friendly name is: {0}",
                 AppDomain.CurrentDomain.FriendlyName
             ));
+            /*
+             * Here I know I am "back" to branch "master"
+             */
+            Console.WriteLine(string.Format(
+                "My 'DomainManager' says the EntryAssembly full name is: {0}",
+                AppDomain.CurrentDomain.DomainManager.EntryAssembly.FullName
+            ));
         }
     }
 }
