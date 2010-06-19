2b99ded8624996dca2656ca0faf2bbe0dfcaae34
DBJ <dbjdbj@gmail.com>
2010-06-19 02:37:31 +0100

    string.each() added

--- monadic.cs
+++ monadic.cs
@@ -37,15 +37,36 @@ namespace dbj.mad
                     .Begin(s => s + " processed by trivial proto-monad.")
                     ("another", s => s + " And it's chainable!");
             // 2010 06 17 15:43:13  DBJ     Stunt programming in C#4
+
+          result = result.each(
+               (c, i) => { return '#'; }
+               );
         }
     }
 
-    static class lib
+    /// <summary>
+    /// DBJ string extensions
+    /// </summary>
+    static class str
     {
         static readonly System.Globalization.CultureInfo cc_ = System.Globalization.CultureInfo.CurrentCulture;
+        
         public static string format(this string argument, params object[] args)
         {
-            return string.Format(lib.cc_, argument, args);
+            return string.Format(str.cc_, argument, args);
+        }
+
+        public static string each
+            (this string argument, Func<char, int, char > operation, params object[] args)
+        {
+            int idx = 0;
+            var copy = new System.Text.StringBuilder() ;
+            foreach (char c in argument )
+            {
+                    copy.Append(operation(c, idx++));
+                    continue;
+            }
+            return copy.ToString() ;
         }
     }
 }
